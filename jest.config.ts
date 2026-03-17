3faa1c101f9317dbe64b65230cfae51640df723e
ono9000 <onofreserrano9000@gmail.com>
2026-03-17 11:44:59 +0100

    chore: add react-leaflet, leaflet, @mapbox/polyline dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,9 @@ const createJestConfig = nextJest({ dir: './' })
 const config: Config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(react-leaflet|@react-leaflet/core|leaflet)/).*',
+  ],
 }
 
 export default createJestConfig(config)
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['react-leaflet', '@react-leaflet/core', 'leaflet'],
+}
 
 export default nextConfig
